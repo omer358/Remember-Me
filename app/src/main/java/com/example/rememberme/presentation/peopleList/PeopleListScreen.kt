d0f496fbdad5ce43b50fff92735e03cc460b2a5a
omer358 <omermaki358@gmail.com>
2024-06-26 01:47:24 +0400

    Resolves #21

--- app/src/main/java/com/example/rememberme/presentation/peopleList/PeopleListScreen.kt
+++ app/src/main/java/com/example/rememberme/presentation/peopleList/PeopleListScreen.kt
@@ -26,6 +26,7 @@ import com.example.rememberme.R
 import com.example.rememberme.data.FakeDataSource
 import com.example.rememberme.domain.model.People
 import com.example.rememberme.presentation.common.composables.PeopleListItem
+import com.example.rememberme.presentation.peopleList.composable.EmptyStateScreen
 import com.example.rememberme.ui.theme.RememberMeTheme
 
 @Composable
@@ -33,7 +34,7 @@ fun PeopleScreen(
     modifier: Modifier = Modifier,
     viewModel: PeopleViewModel = hiltViewModel(),
     navigateToDetailScreen: (Long) -> Unit,
-    navigateToAddNewPersonScreen:() -> Unit
+    navigateToAddNewPersonScreen: () -> Unit
 ) {
     val people = viewModel.people.collectAsState(initial = emptyList())
     PeopleScreenContent(people, modifier, navigateToDetailScreen, navigateToAddNewPersonScreen)
@@ -69,16 +70,20 @@ fun PeopleScreenContent(
             }
         }
     ) { it ->
-        LazyColumn(
-            modifier = modifier
-                .padding(it)
-                .fillMaxSize()
-                .testTag("PeopleListScreen")
-        ) {
-            items(count = peopleState.value.size) { index ->
-                PeopleListItem(peopleState.value[index], { personId ->
-                    navigateToDetailScreen(personId)
-                })
+        if (peopleState.value.isEmpty()) {
+            EmptyStateScreen(modifier = modifier.padding(paddingValues = it))
+        } else {
+            LazyColumn(
+                modifier = modifier
+                    .padding(it)
+                    .fillMaxSize()
+                    .testTag("PeopleListScreen")
+            ) {
+                items(count = peopleState.value.size) { index ->
+                    PeopleListItem(peopleState.value[index], { personId ->
+                        navigateToDetailScreen(personId)
+                    })
+                }
             }
         }
     }
---
+++ app/src/main/java/com/example/rememberme/presentation/peopleList/composable/EmptyStateScreen.kt
@@ -0,0 +1,52 @@
+package com.example.rememberme.presentation.peopleList.composable
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.rememberme.R
+import com.example.rememberme.ui.theme.RememberMeTheme
+
+@Composable
+fun EmptyStateScreen(
+    modifier: Modifier = Modifier
+){
+    Column (
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.ic_empty_state_vector),
+            contentDescription = null,
+            modifier = Modifier.size(300.dp)
+        )
+        Text(
+            text = "Go out and Be social!",
+            style = MaterialTheme.typography.titleLarge
+        )
+    }
+
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun EmptyStateScreenPreview(){
+    RememberMeTheme {
+        Surface {
+            EmptyStateScreen()
+        }
+    }
+}
