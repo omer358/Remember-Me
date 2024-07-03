a4767007d0a117a6a47f077ca942c722e0d0930a
omer358 <omermaki358@gmail.com>
2024-07-04 02:55:13 +0400

    fix the bug within ValidateNamesUseCase

--- app/src/main/java/com/example/rememberme/domain/usecases/add_person/ValidateNamesUseCase.kt
+++ app/src/main/java/com/example/rememberme/domain/usecases/add_person/ValidateNamesUseCase.kt
@@ -4,10 +4,16 @@ import com.example.rememberme.domain.model.ValidationResult
 
 class ValidateNamesUseCase {
     operator fun invoke(name: String): ValidationResult {
-        if (name.isBlank() && name.length >= 2) {
+        if (name.isBlank()) {
             return ValidationResult(
                 successful = false,
                 errorMessage = " this field cannot be blank"
+            )
+        }
+        if (name.length < 2) {
+            return ValidationResult(
+                successful = false,
+                errorMessage = " this field must be at least 2 characters long"
             )
         }
         return ValidationResult(successful = true)
